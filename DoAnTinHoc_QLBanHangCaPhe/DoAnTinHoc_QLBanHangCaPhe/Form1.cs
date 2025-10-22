c672fe9dd6bf648d2e252633cd9ca21bd663872f
LeTranNgocGiau <DH52200607@student.stu.edu.vn>
2025-10-23 01:29:39 +0700

    Hoan thanh yeu cau : doc, ghi file csv

---
+++ DoAnTinHoc_QLBanHangCaPhe/DoAnTinHoc_QLBanHangCaPhe/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+
+namespace DoAnTinHoc_QLBanHangCaPhe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dulieudocduoc = XuLyCSV.DocFile();
+            if(dulieudocduoc == "Lỗi đọc file")
+            {
+                MessageBox.Show("Lỗi không tìm thấy file Coffe_sales.csv", "Lỗi file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("Đã đọc tiêu đề: "+ dulieudocduoc, "Kết quả đọc");
+
+            if (XuLyCSV.GhiFile(dulieudocduoc))
+            {
+                MessageBox.Show("Ghi file Baocao_Tuan1.txt thành công", "Thành công!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Ghi file Không Thành Công!!", "Lỗi !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+    }
+}
---
+++ DoAnTinHoc_QLBanHangCaPhe/DoAnTinHoc_QLBanHangCaPhe/XuLyCSV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnTinHoc_QLBanHangCaPhe
+{
+    public static class XuLyCSV
+    {
+        public static string DocFile()
+        {
+            string filePath = "Coffe_sales.csv";
+            if (!File.Exists(filePath))
+            {
+                return "Lỗi đọc file";
+            }
+            string[]lines = File.ReadAllLines(filePath);
+            if(lines.Length > 0)
+            {
+                return lines[0];
+            }
+            return "File rỗng";
+        }
+
+        public static bool GhiFile(string content)
+        {
+            string summaryFilePath = "Baocao_Tuan1.txt";
+            try
+            {
+                File.WriteAllText(summaryFilePath, "\n Bao cao da duoc ghi thanh cong. Du lieu doc duoc: " + content);
+                return true;
+            } catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
